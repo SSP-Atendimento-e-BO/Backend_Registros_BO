d05c1705839469d3e5d2414741533701c0e97fc2
carlosEduardo-hub <carloseduardo010616@gmail.com>
2025-10-27 20:59:37 -0300

    chore: update file extensions from .js to .ts in db module

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
-import { schema } from './schema/index.js'
-import { env } from '../env.js'
+import { schema } from './schema/index.ts'
+import { env } from '../env.ts'
 
 export const sql = postgres(env.DATABASE_URL)
 export const db = drizzle(sql, {
